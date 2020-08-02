34b295e80da6850e7a8fe9596a3fb0aca09391dd
Jupiter <jupitercc@protonmail.com>
2020-08-02 02:01:49 +0100

    -

--- index.js
+++ index.js
@@ -2,37 +2,24 @@ const { Telegraf } = require('telegraf')
 const fs = require('fs')
 const { v4: uuidv4 } = require('uuid');
 const sharp = require("sharp")
-const LocalSession = require('telegraf-session-local');
+const db = require('better-sqlite3')('db.sqlite3')
 
 const { createSVGWindow, config } = require('svgdom')
-
-
-const window = createSVGWindow()
-const document = window.document
 const { SVG, registerWindow } = require('@svgdotjs/svg.js')
-// register window and document
-registerWindow(window, document)
-
-// create canvas
-
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
-var session = new LocalSession({ database: 'db.json' })
-bot.use(session.middleware())
-
 function getSid(msg) {
-    return `${msg.chat.id}:${msg.from.id}`
+  return `${msg.from.id}:${msg.chat.id}`
 }
 
-function inc(sid, countername) {
-  var repliedSession = session.getSession(sid)
-  !('counter' in repliedSession) && (repliedSession.counter = {})
-  // repliedSession.counter = repliedSession.counter || {}
-  repliedSession.counter[countername] = repliedSession.counter[countername] || 0
-  repliedSession.counter[countername]++
-  session.saveSession(sid, repliedSession)
-  return repliedSession.counter[countername]
+function iinc(sid, countername) {
+  var count = db.prepare("SELECT Count count FROM gcounters WHERE name  = ?").get(countername)
+  db.prepare("UPDATE gcounters SET count = ? WHERE name = ?").run(count.count+1, countername)
+  console.log(count)
+  return count.count+1
+
+
 }
 
 var svgs = {
@@ -40,27 +27,50 @@ var svgs = {
   'slam': fs.readFileSync('slam.svg', 'utf8'),
 }
 
-bot.hears('pap', (ctx) => {
+bot.hears(new RegExp('^pap$'), (ctx) => {
   if (ctx.update.message.reply_to_message) {
+    var window = createSVGWindow()
+    var document = window.document
+    registerWindow(window, document)
     var reply_to_message = ctx.update.message.reply_to_message
     var repliedSid = getSid(reply_to_message)
-    var now = inc(repliedSid, 'pap')
+    var incd = iinc(repliedSid, 'pap')
     var draw = SVG(document.documentElement)
     draw.svg(svgs['pap'])
-    draw.findOne('#tspan869').text(`${now} PAPS!`)
-    draw.findOne('#tspan875').text(ctx.message.from.firstName)
+    draw.findOne('#tspan869').text(`${incd} PAPS!`)
+    draw.findOne('#tspan875').text(reply_to_message.from.first_name)
 
-    sharp(Buffer.from(draw.svg(), 'utf8')).webp().toBuffer()
+    sharp(Buffer.from(draw.svg(), 'utf8'), {density: 600}).resize({width: 512}).webp().toBuffer()
       .then(res => {
         ctx.replyWithSticker({
           source: res
         }, {reply_to_message_id: ctx.update.message.reply_to_message.message_id})
       })
 
-    // ctx.replyWithSticker('pap', {reply_to_message_id: ctx.update.message.reply_to_message.message_id})
   }
 })
 
+bot.hears('slam', (ctx) => {
+  if (ctx.update.message.reply_to_message) {
+    var window = createSVGWindow()
+    var document = window.document
+    registerWindow(window, document)
+    var reply_to_message = ctx.update.message.reply_to_message
+    var repliedSid = getSid(reply_to_message)
+    var incd = iinc(repliedSid, 'slam')
+    var draw = SVG(document.documentElement)
+    draw.svg(svgs['slam'])
+    draw.findOne('#tspan845').text(`${incd} SLAMS!`)
+    draw.findOne('#tspan851').text(ctx.message.from.first_name)
 
-bot.start((ctx) => ctx.reply('Welcome'))
+    sharp(Buffer.from(draw.svg(), 'utf8'), {density: 600}).resize({width: 512}).webp().toBuffer()
+      .then(res => {
+        ctx.replyWithSticker({
+          source: res
+        }, {reply_to_message_id: ctx.update.message.reply_to_message.message_id})
+      })
+
+  }
+})
+
 bot.launch()
